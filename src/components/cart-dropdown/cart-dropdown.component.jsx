5ce845cab29f09d2e632939817efcabe7288dd4a
Jitender Sharma <jitendersharma@A7oM.local>
2024-03-22 15:32:09 +0530

    added in our add to cart feature

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,19 @@
+import { useContext } from "react";
 import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
+import { CartContext } from "../../context/cart.context";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        <Button>GO TO CHECKOUT</Button>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
       </div>
+      <Button>GO TO CHECKOUT</Button>
     </div>
   );
 };
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,10 +1,11 @@
 import { useContext } from "react";
 import { ProductContext } from "../../context/products.context";
-import ProductCard from "../../components/product-card/product-cart.component";
-import './shop.styles.scss';
+import ProductCard from "../../components/product-card/product-card.component";
+import "./shop.styles.scss";
 
 const Shop = () => {
   const { products } = useContext(ProductContext);
+
   return (
     <div className="products-container">
       {products.map((product) => (

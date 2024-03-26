bdb18f5a3d6ac57aaad403e8a99afc68a805d581
Jitender Sharma <jitendersharma@A7oM.local>
2024-03-26 18:22:17 +0530

    directory refactor and route setup for directory

--- src/components/directory-item/directory-item.coponent.jsx
+++ src/components/directory-item/directory-item.coponent.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import {
   BackgroundImage,
   Body,
@@ -5,10 +6,13 @@ import {
 } from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
-  const { title, imageUrl } = category;
-
+  const { title, imageUrl, route } = category;
+  const navigate = useNavigate();
+  const onNavigateHandler = () => {
+    navigate(route);
+  };
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
